e7ae0fa58e76b8db49743c197072a18b63471454
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-01-16 06:43:41 +0000

    Added some debug statements

--- drag_and_drop_app/views.py
+++ drag_and_drop_app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     if request.method == 'POST' and request.FILES['the_file']:
-        pass
+        print("It's a POST")
+        print(request.FILES)
     return render(request, 'home.html')
