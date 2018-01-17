e231cf1045cbd8ba7ef67fa4ea5ccedf671531ca
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-01-17 12:53:14 +0000

    Test write upload file to disk

--- drag_and_drop_app/views.py
+++ drag_and_drop_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 def home(request):
     if request.method == 'POST' and request.FILES['the_file']:
         print("It's a POST")
-        print(request.FILES)
+        the_file = request.FILES['the_file']
+        fs = FileSystemStorage()
+        filename = fs.save(the_file.name, the_file)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'home.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
     return render(request, 'home.html')
+
