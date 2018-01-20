9482dc4b4286b4cb5fe81e6e21fe7c857749d4a5
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-01-20 21:22:46 +0000

    Removed debug print

--- drag_and_drop_app/views.py
+++ drag_and_drop_app/views.py
@@ -3,7 +3,6 @@ from django.core.files.storage import FileSystemStorage
 
 def home(request):
     if request.method == 'POST' and request.FILES['the_file']:
-        print("It's a POST")
         the_file = request.FILES['the_file']
         fs = FileSystemStorage()
         filename = fs.save(the_file.name, the_file)
