634cf2993813b5b21d995ef356a38a5157186602
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-06-18 08:24:27 +0100

    Snapshot for different result page WIP

--- drag_and_drop_app/views.py
+++ drag_and_drop_app/views.py
@@ -3,11 +3,13 @@ from django.core.files.storage import FileSystemStorage
 
 def home(request):
     if request.method == 'POST' and request.FILES['file']:
+        #import pdb;
+        #pdb.set_trace()
         the_file = request.FILES['file']
         fs = FileSystemStorage()
         filename = fs.save(the_file.name, the_file)
         uploaded_file_url = fs.url(filename)
-        return render(request, 'home.html', {
+        return render(request, 'result.html', {
             'uploaded_file_url': uploaded_file_url
         })
     return render(request, 'home.html')
